1740b9677d017a49dab2179f84fef27d1ee8c979
batyss <batsy8219@gmail.com>
2021-02-26 15:21:30 +0300

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+const game = new Game();
+game.start();
+
+import Game, { GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './game.js';
+
+console.log('this works too!')
